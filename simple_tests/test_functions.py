091ec6e95416e542f20604742a3162a6fd24af28
Tani Gross <jgross5@mail.yu.edu>
2023-06-20 11:51:26 -0400

    Fix test file not being found Fixes #61

--- simple_tests/test_functions.py
+++ simple_tests/test_functions.py
@@ -1,15 +1,15 @@
-import functions
-import unittest
+from functions import Function
 
-class FunctionTests(unittest.TestCase):
-    def largest_test(self):
-        assert functions.find_largest([1,3,4,3,1,3,6,12,56,63]) == 63
-        assert functions.find_largest([-1,-2,-4,10,-590,100]) == 100
+class TestFunction:
+    def test_largest(self):
+        assert Function.find_largest([1,3,4,3,1,3,6,12,56,63]) == 63
+        assert Function.find_largest([-1,-2,-4,10,-590,100]) == 100
 
-    def smallest_test(self):
-        assert functions.find_smallest([1,3,4,3,1,3,6,12,56,63]) == 1
-        assert functions.find_smallest([-1,-2,-4,10,-590,100]) == -590
+    def test_smallest_test(self):
+        assert Function.find_smallest([1,3,4,3,1,3,6,12,56,63]) == 1
+        assert Function.find_smallest([-1,-2,-4,10,-590,100]) == -590
 
-    def is_integer(self):
-        assert functions.is_integer_in_list(3, [1,2,3,4,5,6,7,8])
-        assert functions.is_integer_in_list(-1, [0,1,2,3,4,5,6,-1])
+    def test_is_integer(self):
+        assert Function.is_integer_in_list(3, [1,2,3,4,5,6,7,8])
+        assert Function.is_integer_in_list(-1, [0,1,2,3,4,5,6,-1])
+
