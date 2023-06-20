8255134a9a9e7f063dc4aaf7b7c6227c56654924
Tani Gross <jgross5@mail.yu.edu>
2023-06-20 11:07:18 -0400

    Delete Tani's student files Fixes #55

--- Tani_Gross/test_studenttest.py
+++
@@ -1,9 +0,0 @@
-import main
-
-def test_create_student():
-    student = main.Student("Tani Gross")
-    assert student.get_classes() == []
-    student.add_class("Math")
-    assert student.get_classes() == ["Math"]
-    student.add_class("Science")
-    assert student.get_classes() == ["Math", "Science"]
